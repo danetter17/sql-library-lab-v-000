f7f78dde1dff345a9c1a1aa7005eefd52f551d17
Daniel Etter <etter.dt@gmail.com>
2018-11-03 19:55:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,16 +26,17 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title FROM (
-    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
-      FROM series
-      JOIN characters
-        ON series.id = characters.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
-      ORDER BY num_species DESC
-      LIMIT 1);"
+      SELECT series.title AS title, COUNT(characters.series_id) AS num_species
+        FROM series
+        JOIN characters
+          ON series.id = characters.series_id
+        WHERE characters.species = 'human'
+        GROUP BY characters.series_id
+        ORDER BY num_species DESC
+        LIMIT 1);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.book_id) AS "
+  "SELECT characters.name, COUNT(character_books.book_id) AS numb_books FROM characters
+      JOIN"
 end
