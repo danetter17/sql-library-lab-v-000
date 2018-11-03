a0f07088dfd90168af422fe20d7eb0a4b5090592
Daniel Etter <etter.dt@gmail.com>
2018-11-03 19:42:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,20 +2,20 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
       INNER JOIN series ON books.series_id = series.id
       WHERE series.id = 1
-      ORDER BY books.year"
+      ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters
       ORDER BY length(motto)
-      DESC LIMIT 1"
+      DESC LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS numb_species FROM characters
       GROUP BY species
       ORDER BY numb_species
-      DESC LIMIT 1"
+      DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
